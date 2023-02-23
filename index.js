e799513e263be38cf89be607f36b9d868a2946c2
PRiMENON <kuroweb@gmail.com>
2023-02-23 18:15:19 +0900

    async function

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
 const glob = require('glob')
 const fs = require('fs')
-const tmpFile = 'tmp.txt';
-const ResultFile = 'lucky-visitor.txt';
+const tmpFile = 'tmp.txt'
+const ResultFile = 'lucky-visitor.txt'
 
 function remove_File(path) {
     if (fs.existsSync(path)) {
-        fs.unlink(path, (err) => {
-            if (err) throw err;
-        })
+        fs.unlink(path, err => { if (err) throw err })
     }
 }
 
-try {
-    // remove file.
-    remove_File(ResultFile);
+async function main() {
+    try {
+        // make base file.
+        let basefile = await fs.readFileSync('./src/ublockorigin.md', 'utf-8')
 
-    // make base file.
-    let basefile = fs.readFileSync('./src/ublockorigin.md', 'utf-8');
-
-    // get current time.
-    const date = new Date;
-    let dateISO = date.toISOString();
-    basefile = basefile.replace('{UPDATE}', dateISO);
-    fs.appendFileSync(ResultFile, basefile, function (err) {
-        if (err) throw err;
-    })
-
-    // get ioc.txt paths.
-    const paths = glob.sync('**/ioc.txt');
-
-    // create temporary file.
-    for (const path of paths) {
-        let list = fs.readFileSync(path, 'utf-8', { flag: 'r' });
-        fs.appendFileSync(tmpFile, list, { flag: 'a' }, function (err) {
-            if (err) throw err;
+        // get current time.
+        const date = new Date()
+        let dateISO = date.toISOString()
+        basefile = basefile.replace('{UPDATE}', dateISO)
+        await fs.appendFileSync(ResultFile, basefile, { flag: 'w'}, function (err) {
+            if (err) throw err
         })
+
+        // get ioc.txt paths.
+        const paths = glob.sync('**/ioc.txt')
+
+        // create temporary file.
+        for (const path of paths) {
+            let list = await fs.readFileSync(path, 'utf-8', { flag: 'r' })
+            await fs.appendFileSync(tmpFile, list, { flag: 'a+' }, function (err) {
+                if (err) throw err
+            })
+        }
+
+        // read temporary file, make filter file.
+        let lists = await fs.readFileSync(tmpFile, 'utf-8', { flag: 'r' }).toString().split('\r\n')
+        lists = await lists.filter((element, index) => lists.indexOf(element) === index)
+        await fs.appendFileSync(ResultFile, lists.join('\n'), { flag: 'a+' }, err => {
+            if (err) throw err
+        })
+    } catch (err) {
+        console.error(err.message)
+    } finally {
+        remove_File(tmpFile)
+        console.log('script completed.')
     }
-
-    // read temporary file, make filter file.
-    let lists = fs.readFileSync(tmpFile, 'utf-8', { flag: 'r' }).toString().split('\r\n');
-    lists = lists.filter((element, index) => lists.indexOf(element) === index);
-    fs.appendFileSync(ResultFile, lists.join('\n'), { flag: 'a+' }, err => {
-        if (err) throw err;
-    });
-
-} catch (err) {
-    console.error(err.message);
-} finally {
-    remove_File(tmpFile);
-    console.log('script completed.')
 }
+
+main()
